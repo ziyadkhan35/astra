b396b041f1af4c5084369f89c138ba4001262a0e
murudovz17 <murudovz17@gmail.com>
2024-03-03 23:31:18 +0400

    adding vercel speed insights

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Document() {
   return (
@@ -14,6 +15,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   );
